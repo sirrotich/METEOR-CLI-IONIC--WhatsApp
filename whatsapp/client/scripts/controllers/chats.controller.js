c65fac1c06160f074e9d9a51a32b77b49a28695d
sirrotich <rotichtitus12@gmail.com>
2019-04-25 03:35:14 +0300

    implemet the controller method which handle the vent

--- whatsapp/client/scripts/controllers/chats.controller.js
+++ whatsapp/client/scripts/controllers/chats.controller.js
@@ -1,3 +1,5 @@
+import Ionic from 'ionic-scripts';
+import { Meteor } from 'meteor/meteor';
 import { Controller } from 'angular-ecmascript/module-helpers';
 import { Chats } from '../../../lib/collections';
  
@@ -5,8 +7,11 @@ export default class ChatCtrl extends Controller {
   constructor() {
     super(...arguments);
  
+
     this.chatId = this.$stateParams.chatId;
- 
+    this.isIOS = Ionic.Platform.isWebView() && Ionic.Platform.isIOS();
+    this.isCordova = Meteor.isCordova;
+
     this.helpers({
       messages() {
         return Messages.find({ chatId: this.chatId });
@@ -19,7 +24,34 @@ export default class ChatCtrl extends Controller {
 
   sendMessage() {
   }
+  inputUp () {
+    if (this.isIOS) {
+      this.keyboardHeight = 216;
+    }
+ 
+    this.scrollBottom(true);
+  }
+ 
+  inputDown () {
+    if (this.isIOS) {
+      this.keyboardHeight = 0;
+    }
+ 
+    this.$ionicScrollDelegate.$getByHandle('chatScroll').resize();
+  }
+ 
+  closeKeyboard () {
+    if (this.isCordova) {
+      cordova.plugins.Keyboard.close();
+    }
+  }
+ 
+  scrollBottom(animate) {
+    this.$timeout(() => {
+      this.$ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(animate);
+    }, 300);
+  }
 }
  
 ChatCtrl.$name = 'ChatCtrl';
-ChatCtrl.$inject = ['$stateParams'];
+ChatCtrl.$inject = ['$stateParams', '$timeout', '$ionicScrollDelegate'];
